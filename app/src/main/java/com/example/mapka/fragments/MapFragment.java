43593f7316237bebf69f726f3e3a7879f724232f
dan.te <fallowdeer.mail@gmail.com>
2019-11-27 21:36:29 +0100

    Google Map features improved, addes Marker pointing on PK IT Institute and current location hadnler + Added permissions for location, sms-send and contact read pop-up.

--- app/src/main/java/com/example/mapka/fragments/MapFragment.java
+++ app/src/main/java/com/example/mapka/fragments/MapFragment.java
@@ -1,5 +1,6 @@
 package com.example.mapka.fragments;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,20 +9,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.mapka.R;
+import com.example.mapka.activities.MainActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.concurrent.Executor;
 
 
 public class MapFragment extends Fragment {
+    private FusedLocationProviderClient fusedLocationClient;
+    static final LatLng katInfPK = new LatLng(50.071211, 19.941409);;
+    LatLng currentLocation;
     MapView mMapView;
     private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
 
@@ -33,23 +48,31 @@ public class MapFragment extends Fragment {
             e.printStackTrace();
         }
 
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
+        fusedLocationClient.getLastLocation().
+                addOnSuccessListener(this.getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                           currentLocation = null;
+                        if (location != null) {
+                            currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMapView.getMapAsync(new OnMapReadyCallback() {
+                                @Override
+                                public void onMapReady(GoogleMap mMap) {
+                                    googleMap = mMap;
+                                    // For showing a move to my location button
+                                    googleMap.setMyLocationEnabled(true);
+                                    // For dropping a marker at a point on the Map
+                                    googleMap.addMarker(new MarkerOptions().position(katInfPK).title("PK II").snippet("Instytut Informatyki PK."));
+//                                    googleMap.addMarker(new MarkerOptions().position(currentLocation).title("Me :)").snippet("This is your current position."));
 
-                // For showing a move to my location button
-                googleMap.setMyLocationEnabled(true);
-
-                // For dropping a marker at a point on the Map
-                LatLng katInfPK = new LatLng(50.071211, 19.941409);
-                googleMap.addMarker(new MarkerOptions().position(katInfPK).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(katInfPK).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
-        });
+                                    // For zooming automatically to the location of the marker
+                                    CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLocation).zoom(12).build();
+                                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                                }
+                            });
+                        }
+                    }
+                });
 
         return rootView;
     }
